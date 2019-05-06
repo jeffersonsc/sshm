1bb29fa3a7fff9c84731dcc8a67bbec842aeb19d
Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>
2019-05-06 15:59:16 -0300

    Prepare structs connections
    Signed-off-by: Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
+
+// Endpoint represent connection interface infos
+type Endpoint struct {
+	Host string
+	Port int
+}
+
+func (e *Endpoint) String() string {
+	return fmt.Sprintf("%s:%d", e.Host, e.Port)
+}
+
+// SSHTunnel data infos for connection
+type SSHTunnel struct {
+	Local  *Endpoint
+	Server *Endpoint
+	Remote *Endpoint
+
+	Config *ssh.ClientConfig
+}
+
 func main() {
 
 }
