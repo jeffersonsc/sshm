d3c53d0fef755da2ae13e41fe8e2ab14dcf30678
Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>
2019-05-06 16:58:01 -0300

    Add pkg ssh tunnel
    Signed-off-by: Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>

--- main.go
+++ main.go
@@ -1,89 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net"
-	"os"
-
 	"github.com/jeffersonsc/sshm/pkg/endpoint"
+	"github.com/jeffersonsc/sshm/pkg/sshtunnel"
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/agent"
 )
-
-// SSHTunnel data infos for connection
-type SSHTunnel struct {
-	Local  *endpoint.Endpoint
-	Server *endpoint.Endpoint
-	Remote *endpoint.Endpoint
-
-	Config *ssh.ClientConfig
-}
-
-func (st *SSHTunnel) forward(localConn net.Conn) {
-	serverConn, err := ssh.Dial("tcp", st.Server.String(), st.Config)
-	if err != nil {
-		fmt.Println("Server dial error: ", err.Error())
-		return
-	}
-
-	remoteConn, err := serverConn.Dial("tcp", st.Remote.String())
-	if err != nil {
-		fmt.Println("Remote dial error: ", err.Error())
-		return
-	}
-
-	copyConn := func(writer, reader net.Conn) {
-		_, err := io.Copy(writer, reader)
-		if err != nil {
-			fmt.Println("io.Copy error: ", err.Error())
-		}
-	}
-
-	go copyConn(localConn, remoteConn)
-	go copyConn(remoteConn, localConn)
-}
-
-// Start local server client
-func (st *SSHTunnel) Start() error {
-	listener, err := net.Listen("tcp", st.Local.String())
-	if err != nil {
-		return err
-	}
-	defer listener.Close()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			return err
-		}
-
-		go st.forward(conn)
-	}
-}
-
-// SSHAgent return autehnticable method
-func SSHAgent() ssh.AuthMethod {
-	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
-		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
-	}
-	return nil
-}
-
-// PublicKeyFile method authentication ssh
-func PublicKeyFile(file string) ssh.AuthMethod {
-	buffer, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil
-	}
-
-	key, err := ssh.ParsePrivateKey(buffer)
-	if err != nil {
-		return nil
-	}
-	return ssh.PublicKeys(key)
-}
 
 func main() {
 	localEndpoint := &endpoint.Endpoint{
@@ -92,28 +13,24 @@ func main() {
 	}
 
 	serverEndpoint := &endpoint.Endpoint{
-		Host: "xx.xxx.xx.xxx",
+		Host: "localhost",
 		Port: 22,
 	}
 
 	remoteEndpoint := &endpoint.Endpoint{
-		Host: "xx.xx.xx.xx",
+		Host: "localhost",
 		Port: 22,
 	}
 
 	sshConfig := &ssh.ClientConfig{
 		User: "user",
-		// Auth: []ssh.AuthMethod{
-		// 	SSHAgent(),
-		// },
 		Auth: []ssh.AuthMethod{
-			// PublicKeyFile("/home/jeffersons/Documentos/AWS-PENS/Telecom2018SP.pem"),
-			PublicKeyFile("/home/USER/.ssh/KEY"),
+			sshtunnel.SSHAgent(),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
-	tunnel := &SSHTunnel{
+	tunnel := &sshtunnel.SSHTunnel{
 		Config: sshConfig,
 		Local:  localEndpoint,
 		Server: serverEndpoint,
---
+++ pkg/sshtunnel/sshtunnel.go
@@ -0,0 +1,88 @@
+package sshtunnel
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+
+	"github.com/jeffersonsc/sshm/pkg/endpoint"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+// SSHTunnel data infos for connection
+type SSHTunnel struct {
+	Local  *endpoint.Endpoint
+	Server *endpoint.Endpoint
+	Remote *endpoint.Endpoint
+
+	Config *ssh.ClientConfig
+}
+
+func (st *SSHTunnel) forward(localConn net.Conn) {
+	serverConn, err := ssh.Dial("tcp", st.Server.String(), st.Config)
+	if err != nil {
+		fmt.Println("Server dial error: ", err.Error())
+		return
+	}
+
+	remoteConn, err := serverConn.Dial("tcp", st.Remote.String())
+	if err != nil {
+		fmt.Println("Remote dial error: ", err.Error())
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		defer writer.Close()
+		defer reader.Close()
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Println("io.Copy error: ", err.Error())
+		}
+	}
+
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+}
+
+// Start local server client
+func (st *SSHTunnel) Start() error {
+	listener, err := net.Listen("tcp", st.Local.String())
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+
+		go st.forward(conn)
+	}
+}
+
+// SSHAgent return autehnticable method
+func SSHAgent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	}
+	return nil
+}
+
+// PublicKeyFile method authentication ssh
+func PublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
